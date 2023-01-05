1a96e6921ac68d6b376268cdf16a7ff1535a3e73
Lee Jong Wook <ljw6388579@naver.com>
2023-01-05 13:44:13 +0900

    20230105

---
+++ BaekJoonEx/src/B1026보물그리디/Main.java
@@ -0,0 +1,31 @@
+package B1026보물그리디;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        Integer[] A = new Integer[N];
+        Integer[] B = new Integer[N];
+
+        for(int i = 0; i < N; i++){
+            A[i] = sc.nextInt();
+        }
+        Arrays.sort(A);
+        for(int i = 0; i < N; i++){
+            B[i] = sc.nextInt();
+        }
+        Arrays.sort(B, Collections.reverseOrder());
+
+        int S = 0;
+        for(int i = 0; i < N; i++){
+            S += A[i]*B[i];
+        }
+        System.out.println(S);
+    }
+}
